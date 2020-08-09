eb227905587edb1e37e2a4807ee0df4da224dbfc
Matt Briggs <mabrigg@microsoft.com>
2020-08-09 15:59:16 -0700

    add user name

--- actions/action-asz-validation/entrypoint.sh
+++ actions/action-asz-validation/entrypoint.sh
@@ -5,7 +5,7 @@ echo "entrypoint running"
 echo "$GITHUB_REPOSITORY"
 
 cd  /usr/local
-git clone https://github.com/mattbriggs/azs-modular-poc.git
+git clone https://github.com/mattbriggs@github.com/mattbriggs/azs-modular-poc.git
 cd azs-modular-poc
 git checkout "$Branch"
 
