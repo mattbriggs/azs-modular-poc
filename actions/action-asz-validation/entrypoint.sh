09b43de907f9df1ed6c32880e9d3056a91e0abcd
Matt Briggs <mabrigg@microsoft.com>
2020-08-09 15:40:25 -0700

    look up one

--- actions/action-asz-validation/entrypoint.sh
+++ actions/action-asz-validation/entrypoint.sh
@@ -3,11 +3,10 @@ set -e
 
 echo "entrypoint running"
 echo "$GITHUB_REPOSITORY"
-echo "$WORKDIR"
 python /usr/local/bin/val_ki_dockeraction.py
 echo "/usr/local/bin/"
 cd /usr/local/bin/
 ls
-echo "/"
-cd /
+echo "/usr/local/"
+cd /usr/local
 ls
