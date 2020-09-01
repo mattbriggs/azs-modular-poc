db9c1e64e284d72e07d8e65d11f2466e7bcc2df7
Matt Briggs <mabrigg@microsoft.com>
2020-09-01 16:39:58 -0700

    fix entry

--- actions/action-asz-validation/entrypoint.sh
+++ actions/action-asz-validation/entrypoint.sh
@@ -5,7 +5,7 @@ echo "entrypoint running"
 echo "$GITHUB_REPOSITORY"
 echo "$GITHUB_WORKSPACE"
 
-cd "$GITHUB_WORKSPACE"
+cd /usr/local
 git clone https://github.com/mattbriggs/azs-modular-poc.git
 cd azs-modular-poc
 git checkout "$GITHUB_WORKSPACE"
